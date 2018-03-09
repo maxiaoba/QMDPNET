25e88216a6161a9bfb6ef87cbfcbfad58223f7c7
maxiaoba <maxiaoba@umich.edu>
2018-03-08 22:16:07 -0800

    same

--- Algo/vpg_transfer.py
+++ Algo/vpg_transfer.py
@@ -163,7 +163,9 @@ class VPG_t(BatchPolopt, Serializable):
         global_vars          = tf.global_variables()
         is_initialized   = sess.run([tf.is_variable_initialized(var) for var in global_vars])
         not_initialized_vars = [v for (v, f) in zip(global_vars, is_initialized) if not f]
-        print([str(i.name) for i in not_initialized_vars]) # only for testing
+        # print([str(i.name) for i in not_initialized_vars]) # only for testing
+        if len(not_initialized_vars):
+            sess.run(tf.variables_initializer(not_initialized_vars))
 
         self.start_worker()
         start_time = time.time()
